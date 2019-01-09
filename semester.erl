9bef93316c7b28fb9d0c44b614eb7ef2f85ccab5
Roland Karlsson <roland.karlsson@bonetmail.com>
2019-01-10 00:53:45 +0100

    New algorithm according to info from Unionen

--- semester.erl
+++ semester.erl
@@ -46,17 +46,19 @@ history(Commands) ->
     First = maps:get(date, hd(Commands)),
     history(Commands,
 	    #{date => First,
-	      days => 0,
-	      acc => 0.0,
+	      weekdays => 0,
+	      work_day_sallary => 0,
+	      work_days_per_year => 250,
+	      sallary_last_year => 0,
 	      bank =>
-		  #{acc => 0,
-		    normal => 0,
-		    pre => 0,
-		    used_pre => 0,
+		  #{days => 0,
 		    saved => 0,
 		    lost => 0,
-		    ps_acc => 0,
-		    ps_sav => 0}
+		    pre_days => 0,
+		    pre_used => 0,
+		    pre_saved => 0,
+		    ps_days => 0,
+		    ps_saved => 0}
 	     },
 	    []).
 
@@ -70,8 +72,7 @@ history(Commands = [#{date := CDate, command := Command} | Tail],
     case Date < CDate of
 	true ->
 	    NextState = advance_one_day(State), 
-	    Bank = maps:get(bank, NextState),
-	    history(Commands, NextState, [{Date,advance,Bank}|Ops]);
+	    history(Commands, NextState, Ops);
 	false ->
 	    NextState = command(Command, State),
 	    Bank = maps:get(bank, NextState),
@@ -80,41 +81,43 @@ history(Commands = [#{date := CDate, command := Command} | Tail],
 
 advance_one_day(State) ->
     NextDate = next_date(maps:get(date, State)),
-    AccPerDay = acc_per_day(maps:get(days, State), element(1, NextDate)),
-    NextAcc = maps:get(acc, State) + AccPerDay,
-    Bank = maps:get(bank, State),
-    NextBank = maps:put(acc, erlang:round(NextAcc), Bank),
-    State#{date => NextDate, acc => NextAcc, bank => NextBank}.
+    State#{date => NextDate}.
     
 next_date(Date) ->
     NextDate = calendar:date_to_gregorian_days(Date)+1,
     calendar:gregorian_days_to_date(NextDate).
 
-acc_per_day(Days, Year) ->
-    Days / days(Year).
-
-days(Year) ->
-    First = calendar:date_to_gregorian_days({Year, 1, 1}),
-    Last = calendar:date_to_gregorian_days({Year, 12, 31}),
-    Last - First + 1.
-
-command({convert}, State) ->
-    add_acc(save_days(State));
+command({add, Days}, State) ->
+    add_bank(days, Days, State);
+command({pre_add, Days}, State) ->
+    add_bank(pre_days, Days, State);
+command({sallary, Sallary}, State) ->
+    State#{work_day_sallary => Sallary};
+command({ps, Days}, State) ->
+    put_bank(ps_days, Days, State);
+command({ps_save, Days}, State) ->
+    put_bank(ps_saved, Days, State);
+command({new_year}, State) ->
+    save_days(compute_work_day_salary(State));
+command({weekdays, Days}, State) ->
+    convert_weekdays(Days, State);
 command({off, Days}, State) ->
     off(Days, State);
-command(nop, State) ->
-    State;
-command({pre, Days}, State = #{bank := Bank}) ->
-    #{pre := Pre} = Bank,
-    State#{bank => Bank#{pre => Pre + Days}};
 command({days, Days}, State) ->
     State#{days => Days};
-command({ps_acc, Days}, State = #{bank := Bank}) ->
-    NewBank = Bank#{ps_acc => Days},
-    State#{bank => NewBank};
-command({ps_sav, Days}, State = #{bank := Bank}) ->
-    NewBank = Bank#{ps_sav => Days},
-    State#{bank => NewBank}.
+command(nop, State) ->
+    State.
+
+convert_weekdays(NewWeekDays, State = #{weekdays := 0}) ->
+    State#{weekdays => NewWeekDays};
+convert_weekdays(NewWeekDays, State = #{weekdays := OldWeekDays, bank := Bank}) ->
+    %% This is a bit hacky way of converting all days in the bank
+    %% according to "nettometoden". Not sure it is actually done this
+    %% way, but it is an approximation of the truth, I think.
+    #{days := Days, saved := Saved} = Bank,
+    Mult = NewWeekDays/OldWeekDays,
+    NewBank = Bank#{days => round(Mult*Days), saved => round(Mult*Saved)},
+    State#{weekdays => NewWeekDays, bank => NewBank}.
 
 off(0, State) ->
     State;
@@ -122,17 +125,23 @@ off(Days, State = #{bank := Bank}) ->
     NewBank = off_one(Bank),
     off(Days-1, State#{bank => NewBank}).
 
-save_days(State = #{bank := Bank}) ->
-    #{normal := Normal, saved := Saved, used_pre := UPre} = Bank,
-    case take_five(Normal) of
+compute_work_day_salary(State) ->
+    State.   %% TODO ---- needs to be implemented
+
+save_days(State = #{bank := Bank0}) ->
+    #{days := Days,
+      saved := Saved,
+      lost := Lost,
+      pre_days := PreDays,
+      pre_saved := PreSaved} = Bank0,
+    Bank = Bank0#{pre_days => 0, pre_saved => PreSaved + PreDays},
+    case take_five(Days) of
 	{0, 0} ->
-	    State;
+	    State#{bank => Bank};
 	{_, 0} ->
-	    NewBank = Bank#{normal := 0, saved := Saved + Normal},
-	    State#{bank => NewBank};
-	{N, P} ->
-	    NewBank = Bank#{normal := 0, saved := Saved + N, used_pre := UPre - P},
-	    State#{bank => NewBank}
+	    State#{bank => Bank#{days := 0, saved := Saved + Days}};
+	{D, P} ->
+	    State#{bank => Bank#{days := 0, saved := Saved + D, lost := Lost + P}}
     end.
 
 take_five(N) when N < 6 ->
@@ -140,18 +149,14 @@ take_five(N) when N < 6 ->
 take_five(N) ->
     {5,N-5}.
 
-add_acc(State = #{acc := RawAcc, bank := Bank}) ->
-    #{acc := Acc, normal := Normal} = Bank,
-    NewBank = Bank#{acc => 0, normal => Normal + Acc},
-    NewRawAcc = RawAcc - Acc,
-    State#{acc => NewRawAcc, bank => NewBank}.
-
-off_one(Bank = #{saved := 0, normal := 0, pre := Pre, used_pre := UPre}) ->
-    Bank#{pre => Pre - 1, used_pre => UPre + 1};
-off_one(Bank = #{saved := 0, normal := Normal}) ->
-    Bank#{normal => Normal -1};
-off_one(Bank = #{saved := Saved}) ->
-    Bank#{saved => Saved - 1}.
+off_one(Bank = #{days := 0, saved := 0, pre_days := 0, pre_saved := PreSaved, pre_used := PreUsed}) ->
+    Bank#{pre_saved => PreSaved - 1, pre_used => PreUsed + 1};
+off_one(Bank = #{days := 0, saved := 0, pre_days := Pre, pre_used := PreUsed}) ->
+    Bank#{pre_days => Pre - 1, pre_used => PreUsed + 1};
+off_one(Bank = #{days := 0, saved := Saved}) ->
+    Bank#{saved => Saved - 1};
+off_one(Bank = #{days := Days}) ->
+    Bank#{days => Days - 1}.
 
 display([], _) ->
     ok;
@@ -169,32 +174,41 @@ display_op(Date, {Op,Arg}) ->
     io:format("~s ~p=~p~n", [pretty_date(Date), Op, Arg]).
 
 display_header() ->
-    io:format("~10s ~10s ~4s ~4s ~4s - ~4s ~4s ~4s - ~4s ~4s~n",
+    io:format("~10s ~10s ~6s ~6s ~6s - ~6s ~6s ~6s - ~6s ~6s~n",
 	      ["", "",
-	       "Acc", "Norm", "Pre",
-	       "Sav", "Lost", "UPre",
-	       "Pacc", "Psav"]).
+	       "Days", "Saved", "Lost",
+	       "PrD", "PrS", "PrUsd",
+	       "PsD", "PsS"]).
 
 display_bank(Date, Bank, PrevBank) ->
     case Bank == PrevBank of
 	true ->
 	    ignore;
 	false ->
-	    Acc = maps:get(acc, Bank),
-	    Normal = maps:get(normal, Bank),
-	    Pre = maps:get(pre, Bank),
+	    Days = maps:get(days, Bank),
 	    Saved = maps:get(saved, Bank),
 	    Lost = maps:get(lost, Bank),
-	    UPre = maps:get(used_pre, Bank),
-	    Pacc = maps:get(ps_acc, Bank),
-	    Psav = maps:get(ps_sav, Bank),
+	    PreD = maps:get(pre_days, Bank),
+	    PreS = maps:get(pre_saved, Bank),
+	    PreU = maps:get(pre_used, Bank),
+	    PsD = maps:get(ps_days, Bank),
+	    PsS = maps:get(ps_saved, Bank),
 
-	    io:format("~s ~10s ~4w ~4w ~4w - ~4w ~4w ~4w - ~4w ~4w~n",
+	    io:format("~s ~10s ~6w ~6w ~6w - ~6w ~6w ~6w - ~6w ~6w~n",
 		      [pretty_date(Date), "",
-		       Acc, Normal, Pre,
-		       Saved, Lost, UPre,
-		       Pacc, Psav])
+		       Days, Saved, Lost,
+		       PreD, PreS, PreU,
+		       PsD, PsS])
     end.
 
 pretty_date({Year, Month, Day}) ->
     io_lib:format("~p-~2..0w-~2..0w", [Year, Month, Day]).
+
+put_bank(Key, Value, State = #{bank := Bank}) ->
+    State#{bank => maps:put(Key, Value, Bank)}.
+
+add_bank(Key, Value, State = #{bank := Bank}) ->
+    State#{bank => add_map(Key, Value, Bank)}.
+
+add_map(Key, Value, Map) ->
+    maps:put(Key, maps:get(Key, Map, 0) + Value, Map).
